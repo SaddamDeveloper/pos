4b9b7c346fcd6a935f1dc87a4a4baa1be6f9e44a
Saddam Hussain <sishack8@gmail.com>
2020-07-03 22:46:36 +0530

    First Commit

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (Auth::guard($guard)->check()) {
+            switch ($guard) {
+               case 'admin':
+                   $route = 'admin/dashboard';
+                   break;
+               case 'employee':
+                   $route = 'employee/dashboard';
+                   break;
+              default:
+                   $route = 'web.userLoginForm';
+           }
+           return redirect($route);
+       }
+
+        return $next($request);
+    }
+}
